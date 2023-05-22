d92596f11727865a08bc76fc301a9e658c36d6d5
ThierryRaktomanana <ThierryRktm@gmail.com>
2023-05-22 20:30:29 +0300

    style: icon's color change by mode(dark/light)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+let angularIcon = document.getElementById('angular');
+
+function dark(){
+    angularIcon.src = "./img/angular.svg";
+}
+
+function light(){
+    angularIcon.src = "./img/angular_solidBlack.svg";
+}
 
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
@@ -21,9 +30,11 @@ themeToggleBtn.addEventListener('click', function() {
         if (localStorage.getItem('color-theme') === 'light') {
             document.documentElement.classList.add('dark');
             localStorage.setItem('color-theme', 'dark');
+            dark();
         } else {
             document.documentElement.classList.remove('dark');
             localStorage.setItem('color-theme', 'light');
+            light();
         }
 
     // if NOT set via local storage previously
@@ -31,9 +42,11 @@ themeToggleBtn.addEventListener('click', function() {
         if (document.documentElement.classList.contains('dark')) {
             document.documentElement.classList.remove('dark');
             localStorage.setItem('color-theme', 'light');
+            light();
         } else {
             document.documentElement.classList.add('dark');
             localStorage.setItem('color-theme', 'dark');
+            dark();
         }
     }
     
