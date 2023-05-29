be702c89d838f021aea0d1bdc2ef1bdae522ed63
ThierryRaktomanana <ThierryRktm@gmail.com>
2023-05-29 20:02:11 +0300

    style(radial-gradient): add specific animation for svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,21 @@ module.exports = {
   darkMode: 'class',
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation : {
+        colorProgress : 'colorProgress 1s linear infinite' ,
+      },
+      keyframes : {
+        colorProgress : {
+          '0%': { fill: "url(#gradient-ring)"},
+          '20%': { fill: "url(#gradient-ring1)"},
+          '30%': { fill: "url(#gradient-ring2)"},
+          '50%': { fill: "url(#gradient-ring3)"},
+          '70%': { fill: "url(#gradient-ring4)"},
+          '100%': { fill: "url(#gradient-ring5)" },
+        }
+      }
+    },
   },
   plugins: [],
 }
-
